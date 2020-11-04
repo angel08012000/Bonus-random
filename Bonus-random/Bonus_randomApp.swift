4fb0a328c24e3c61dafd3b87bb4543c629ea5175
cih <angel08012000@gmail.com>
2020-11-04 10:21:54 +0800

    Initial Commit

---
+++ Bonus-random/Bonus_randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bonus_randomApp.swift
+//  Bonus-random
+//
+//  Created by 慈慈 on 2020/11/4.
+//
+
+import SwiftUI
+
+@main
+struct Bonus_randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bonus-random/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bonus-random
+//
+//  Created by 慈慈 on 2020/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
